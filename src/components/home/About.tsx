b2e50a3678ad0ba54dc8ac7978da2739ac55e2ae
aaakash06 <akashbagale182@gmail.com>
2024-07-15 11:43:37 +0545

    deployment error resolved

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -2,66 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { GiBiceps } from "react-icons/gi";
 import Image from "next/image";
-const stories = [
-  {
-    title: "The journey to relaxation.",
-    description:
-      "Finding a hammock you can truly relax in didn’t happen overnight. It started with a chance discovery while on vacation, and took a lot of hard work (and a lot of hanging around) to bring the softest, most comfortable, and thoughtfully crafted hammocks to your backyard.",
-    image: "https://cdn.easyfrontend.com/pictures/featured/featured_13.png",
-  },
-  {
-    title: "The way to heaven.",
-    description:
-      "More off this less hello salamander lied porpoise much over tightly circa horse taped so innocuously outside crud mightily rigorous negative one inside gorilla and drew humbly shot tortoise inside opaquely. Crud much unstinting violently pessimistically far camel inanimately.",
-    image: "https://cdn.easyfrontend.com/pictures/about/about9.jpg",
-  },
-];
-
-const StoryItem = ({ item, index }) => {
-  const { title, description, image } = item;
-  return (
-    <>
-      <div
-        className={`col-span-12 md:col-span-5 ${
-          index % 2 === 0
-            ? "order-1 md:order-2 md:col-start-7"
-            : "order-2 md:order-1 md:col-start-2"
-        }`}
-      >
-        <div
-          className={`flex flex-col justify-center ${
-            index % 2 === 0 ? "lg:pl-14" : "lg:pr-14"
-          }`}
-        >
-          <h4 className="text-2xl font-bold mb-4">{title}</h4>
-          <p className="text-base leading-relaxed text-justify opacity-70 mb-0 md:pr-6">
-            {description}
-          </p>
-        </div>
-      </div>
-      <div
-        className={`${
-          index % 2 === 0
-            ? "order-1 md:col-start-2"
-            : "order-1 md:order-2 md:col-start-7"
-        } col-span-12 md:col-span-5  mb-6 md:mb-0 mt-6 md:mt-0`}
-      >
-        <div>
-          <img
-            src={image}
-            alt={title}
-            className="max-w-full h-auto rounded-2xl"
-          />
-        </div>
-      </div>
-    </>
-  );
-};
-
-StoryItem.propTypes = {
-  item: PropTypes.object.isRequired,
-  index: PropTypes.number.isRequired,
-};
 
 export default function About() {
   return (
